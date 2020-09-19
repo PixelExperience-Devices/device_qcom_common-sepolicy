f6891e6f99310d4a15234e63e29bbb16d9f926d0
Pig <pig.priv@gmail.com>
2020-09-19 09:41:05 +0800

    sepolicy_vndr: Remove QCOM guards
    Change-Id: I0efd0b96f45ecfa9eec0b98087f0582dcd282798
    Signed-off-by: chrisl7 <wandersonrodriguesf1@gmail.com>

--- sepolicy_vndr/SEPolicy.mk
+++ sepolicy_vndr/SEPolicy.mk
@@ -1,5 +1,4 @@
 # Board specific SELinux policy variable definitions
-ifeq ($(call is-vendor-board-platform,QCOM),true)
 SEPOLICY_PATH:= device/qcom/sepolicy_vndr
 QSSI_SEPOLICY_PATH:= device/qcom/sepolicy
 BOARD_SYSTEM_EXT_PREBUILT_DIR := device/qcom/sepolicy/generic
@@ -45,6 +44,4 @@ ifeq (,$(filter sdm845 sdm710, $(TARGET_BOARD_PLATFORM)))
     BOARD_VENDOR_SEPOLICY_DIRS += $(SEPOLICY_PATH)/qva/vendor/test
     BOARD_VENDOR_SEPOLICY_DIRS += $(SEPOLICY_PATH)/qva/vendor/test/sysmonapp
     endif
-endif
-
 endif
